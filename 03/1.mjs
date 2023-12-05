bbe690d7efe8ef29ca844b62edb1e59ab2c2bef3
Nikoloz Razmadze <nikoloz@razmadze.me>
2023-12-05 16:04:12 +0400

    Optimize day 3 part 1

--- 03/1.mjs
+++ 03/1.mjs
@@ -2,14 +2,16 @@ import { getLines } from "../read-input.mjs";
 
 const grid = getLines("./input.txt");
 
-const isPartNumber = (grid, line, start, end) => {
-  let out = "";
+const getLine = (line, start, end) => {
   const clampedStart = Math.max(0, start - 1);
-  out += grid[line - 1]?.slice(clampedStart, end + 1) ?? "";
-  out += grid[line + 1]?.slice(clampedStart, end + 1) ?? "";
-  out += grid[line][end] ?? "";
-  out += grid[line][start - 1] ?? "";
-  return !!out.match(/[^\.]/);
+  return grid[line]?.slice(clampedStart, end + 1) ?? "";
+};
+
+const isPartNumber = (line, start, end) => {
+  let out = getLine(line - 1, start, end);
+  out += getLine(line, start, end);
+  out += getLine(line + 1, start, end);
+  return !!out.match(/[^.\d]/);
 };
 
 let sum = 0;
@@ -17,7 +19,7 @@ let sum = 0;
 grid.forEach((line, li) => {
   const matches = [...line.matchAll(/\d+/g)];
   matches.forEach((match) => {
-    if (isPartNumber(grid, li, match.index, match.index + match[0].length)) {
+    if (isPartNumber(li, match.index, match.index + match[0].length)) {
       sum += +match[0];
     }
   });
