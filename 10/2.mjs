a662df7df0259846dbe360c1652fd51a4a09ed49
Nikoloz Razmadze <nikoloz@razmadze.me>
2023-12-11 16:55:44 +0400

    Simplify day 10 part 2

--- 10/2.mjs
+++ 10/2.mjs
@@ -1,38 +1,39 @@
-import { getCharacters, getLine } from "../read-input.mjs";
+import { getLine } from "../read-input.mjs";
 
-const filename = "./input.txt";
-const { line, width } = getLine(filename);
-const data = getCharacters(filename);
+const { width, line } = getLine("./input.txt");
 
-const directions = {
-  "|": { x: 0, y: 0 },
-  "-": { x: 0, y: 0 },
-  L: { x: 1, y: -1 },
-  J: { x: -1, y: -1 },
-  F: { x: 1, y: 1 },
-  7: { x: -1, y: 1 },
+const getStartingPoints = (index) => {
+  // Needs more checks, but works on given input
+  const top = ["|", "F", "7"].includes(line[index - width]) && index - width;
+  const bot = ["|", "J", "L"].includes(line[index + width]) && index + width;
+  const left = ["-", "F", "L"].includes(line[index - 1]) && index - 1;
+  const right = ["-", "J", "7"].includes(line[index + 1]) && index + 1;
+
+  return [top, bot, left, right].find((_) => _);
 };
 
-const getNextDirection = (previous, current) => {
+const getNext = (current, previous) => {
+  switch (line[current]) {
+    case "|":
+      return current > previous ? current + width : current - width;
+    case "-":
+      return current > previous ? current + 1 : current - 1;
+    case "L":
+      return current > previous ? current + 1 : current - width;
+    case "7":
+      return current > previous ? current + width : current - 1;
+    case "J":
+      return current - previous > 1 ? current - 1 : current - width;
+    case "F":
+      return previous - current > 1 ? current + 1 : current + width;
+  }
+};
+
+const getCoordinates = (number) => {
   return {
-    x: previous.x + current.x,
-    y: previous.y + current.y,
+    x: number % width,
+    y: Math.floor(number / width),
   };
-};
-
-const move = (x, y, direction) => {
-  const [origX, origY] = [x, y];
-  const loop = [];
-  do {
-    direction = getNextDirection(
-      direction,
-      directions[data[y]?.[x]] ?? directions["-"]
-    );
-    x += direction.x;
-    y += direction.y;
-    loop.push({ x, y });
-  } while (origX !== x || origY !== y);
-  return loop;
 };
 
 const shoelace = (points) => {
@@ -40,18 +41,28 @@ const shoelace = (points) => {
   const l = points.length;
 
   for (let i = 0; i < l - 1; i++) {
-    sum += points[i].x * points[i + 1].y - points[i].y * points[i + 1].x;
+    const a = getCoordinates(points[i]);
+    const b = getCoordinates(points[i + 1]);
+    sum += a.x * b.y - a.y * b.x;
   }
 
-  sum += points[l - 1].x * points[0].y - points[0].x * points[l - 1].y;
+  const a = getCoordinates(points[l - 1]);
+  const b = getCoordinates(points[0]);
+  sum += a.x * b.y - b.x * a.y;
 
   return Math.abs(sum) / 2;
 };
 
-const start = line.indexOf("S");
-const startX = start % width;
-const startY = Math.floor(start / width);
+const index = line.indexOf("S");
+let current = getStartingPoints(index);
+let previous = index;
 
-const loop = move(startX, startY, { x: 1, y: 0 });
+const loop = [index];
+while (current !== index) {
+  loop.push(current);
+  const temp = current;
+  current = getNext(current, previous);
+  previous = temp;
+}
 
 console.log(shoelace(loop) + 1 - loop.length / 2);
